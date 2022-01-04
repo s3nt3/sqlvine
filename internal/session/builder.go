1b46f472eef97c63196c808031b8a7bf73c3e0d9
Zengxian Ding <dingzengxian@pingcap.com>
2022-01-04 10:40:39 +0800

    session: use mocktikv as kv storage

--- internal/session/builder.go
+++ internal/session/builder.go
@@ -2,14 +2,26 @@ package session
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/pingcap/tidb/domain"
+	"github.com/pingcap/tidb/executor"
 	"github.com/pingcap/tidb/infoschema"
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/model"
 	"github.com/pingcap/tidb/planner/core"
 	"github.com/pingcap/tidb/sessionctx"
+	"github.com/pingcap/tidb/store/mockstore"
 	"github.com/pingcap/tidb/types"
+	"github.com/pingcap/tidb/util/mock"
+	"github.com/s3nt3/sqlvine/internal/logger"
 )
+
+func init() {
+	_ = executor.GlobalMemoryUsageTracker
+	_ = executor.GlobalDiskUsageTracker
+}
 
 type TiDBPlanBuilder struct {
 	ctx     context.Context
@@ -17,10 +29,33 @@ type TiDBPlanBuilder struct {
 	schema  infoschema.InfoSchema
 }
 
+func sessionCtx() sessionctx.Context {
+	ctx := mock.NewContext()
+	// driver := mockstore.EmbedUnistoreDriver{}
+	// kvstore, err := driver.Open("unistore:///tmp/mock/tikv")
+	driver := mockstore.MockTiKVDriver{}
+	kvstore, err := driver.Open("mocktikv:///tmp/mock/tikv")
+	if err != nil {
+		logger.L.Panic(err.Error())
+	}
+	ctx.Store = kvstore
+
+	ctx.GetSessionVars().CurrentDB = "test"
+	ctx.GetSessionVars().SnapshotTS = uint64((time.Now().UnixNano() / int64(time.Millisecond)) << 18)
+
+	do := &domain.Domain{}
+	if err := do.CreateStatsHandle(ctx); err != nil {
+		panic(fmt.Sprintf("create mock context panic: %+v", err))
+	}
+	domain.BindDomain(ctx, do)
+
+	return ctx
+}
+
 func NewTiDBPlanBuilder(tables []*model.TableInfo) *TiDBPlanBuilder {
 	return &TiDBPlanBuilder{
 		ctx:     context.Background(),
-		session: core.MockContext(),
+		session: sessionCtx(),
 		schema:  infoschema.MockInfoSchema(tables),
 	}
 }
